1a34c97ee4624d47eea5f672a3e8f98704e7fd21
Casey Dierking <casey@leantech.com>
2019-04-12 09:57:51 -0500

    cycle light demo with config file

---
+++ cycleLightDemo/index.js
@@ -0,0 +1,31 @@
+const gpio = require('onoff').gpio
+const light = new gpio(12, 'out');
+
+
+const sleep = (howLong) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, howLong)
+    })
+}
+
+const cycleLight = async () => {
+    while (true){
+    light.writeSync(1);
+    await sleep(3000)
+    light.writeSync(0)
+    await sleep(1000)
+    }
+}
+
+const lightOff = () => {
+    light.writeSync(0)
+}
+
+//Handle Ctrl+C to exit cleanly
+process.on('SIGINT', () => {
+    lightOff()
+    process.exit();
+})
+
+lightOff();
+cycleLight();
